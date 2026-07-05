63a811159c2bf3606a7724d02dafebc4a39dbeeb
Bojan Dulevski <bojan.deuleto7@gmail.com>
2026-07-05 14:56:58 +0200

    Update wsgi.py

--- sportivaBaseDjango/sportivaBaseDjango/wsgi.py
+++ sportivaBaseDjango/sportivaBaseDjango/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sportivaBaseDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sportivaBaseDjango.sportivaBaseDjango.settings')
 
 application = get_wsgi_application()
